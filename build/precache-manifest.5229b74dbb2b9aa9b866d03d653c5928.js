1f8cfb7cd5a4974723d7ac3b956d9154df7ff75f
David Nguyen <david@netis.be>
2019-09-15 22:42:23 +0200

    create react app

---
+++ build/precache-manifest.5229b74dbb2b9aa9b866d03d653c5928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cee2e312cdf002b2a6174cd92ba561f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f7b0855ed243a16f938",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1abf9a3cc0c79594dd48",
+    "url": "/static/js/2.8e206acd.chunk.js"
+  },
+  {
+    "revision": "2f7b0855ed243a16f938",
+    "url": "/static/js/main.72926e60.chunk.js"
+  },
+  {
+    "revision": "53ea5a69c130e0645f18",
+    "url": "/static/js/runtime~main.d8491485.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
